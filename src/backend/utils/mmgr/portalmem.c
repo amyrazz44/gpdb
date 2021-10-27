f4946edc4c42c699c702250962a83d4a525b4929
Huiliang Liu <lhuiliang@vmware.com>
2021-10-27 10:58:34 +0800

    Fix is_parallel field in pg_cursors
    1. add is_parallel for pg_cursor in pg_proc.h
    
    2. fix is_parallel field value in "select * from pg_cursors"
       the root cause is the transform from flag 0x400 to bool is unexpected,
    which causes the is_parallel field in pg_cursors is wrong for parallel
    retrieve cursor.

--- src/backend/utils/mmgr/portalmem.c
+++ src/backend/utils/mmgr/portalmem.c
@@ -1253,7 +1253,8 @@ pg_cursor(PG_FUNCTION_ARGS)
 		values[3] = BoolGetDatum(portal->cursorOptions & CURSOR_OPT_BINARY);
 		values[4] = BoolGetDatum(portal->cursorOptions & CURSOR_OPT_SCROLL);
 		values[5] = TimestampTzGetDatum(portal->creation_time);
-		values[6] = BoolGetDatum(PortalIsParallelRetrieveCursor(portal));
+		bool is_parallel = PortalIsParallelRetrieveCursor(portal) ? true : false;
+		values[6] = BoolGetDatum(is_parallel);
 
 		tuplestore_putvalues(tupstore, tupdesc, values, nulls);
 	}

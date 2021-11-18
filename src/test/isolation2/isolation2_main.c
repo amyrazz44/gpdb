cloudberrydb/cloudberrydb

Parallel Retrieve Cursor feature (#12446)
parallel retrieve cursor support

This feature allows that after a cursor is declared, clients could fetch the
tuples on segments in parallel. Previously all tuple fetch operations must be
through the coordinator which could be a bottleneck in some scenarios. We
define a new cursor syntax "PARALLEL RETRIEVE CURSOR" and define a new
corresponding "RETRIEVE" sql syntax to fetch the tuples on segments in
parallel. Typically users also need to specify gp_retrieve_conn as true (e.g.
PGOPTIONS='-c gp_retrieve_conn=true') to set up a retrieve connection and that
connection can be used for RETRIEVE only. Additional token based authentication
is used for the retrieve connection.

More examples and design document please see src/backend/cdb/endpoint/README.
This feature could be used in many scenarios, for example federation query,
utility/tool that needs to access the data in gpdb in parallel.

Co-Authored-By: Adam Lee <ali@pivotal.io>
Co-Authored-By: Chen Mulong <chenmulong@gmail.com>
Co-Authored-By: (Jerome)Junfeng Yang <jeyang@pivotal.io>
Co-Authored-By: Heikki Linnakangas <hlinnakangas@pivotal.io>
Co-Authored-By: Huiliang Liu <lhuiliang@vmware.com>
Co-Authored-By: Max Yang <myang@pivotal.io>
Co-Authored-By: Ming LI <mli@apache.org>
Co-Authored-By: Peifeng Qiu <pqiu@pivotal.io>
Co-Authored-By: Tingfang Bao <bbao@pivotal.io>
Co-Authored-By: Wen Lin <wlin@pivotal.io>
Co-Authored-By: Xiaoran Wang <xiwang@pivotal.io>

Reviewed-by: Alexander Denissov, Ashuka Xue, Ashwin Agrawal,
             Bradford D. Boyle, Gang Xiong, Guo Jian, yozol@github

src/test/isolation2/isolation2_main.c
@@ -109,8 +109,9 @@ isolation_start_test(const char *testname,
 						   "%s ", launcher);
 
 	snprintf(psql_cmd + offset, sizeof(psql_cmd) - offset,
-			 "python3 ./sql_isolation_testcase.py --dbname=\"%s\" < \"%s\" > \"%s\" 2>&1",
+			 "python3 ./sql_isolation_testcase.py --dbname=\"%s\" --initfile_prefix=\"%s\" < \"%s\" > \"%s\" 2>&1",
 			 dblist->str,
+			 outfile,
 			 infile,
 			 outfile);
 

amyrazz44/gpdb

Based on the cherry-picked commit 7a1ce379c12a56a4708568c94bbe8a053a4074ce.
Modified to align with the gp6 logic.
To add more...

src/test/isolation2/isolation2_main.c
@@ -107,8 +107,9 @@ isolation_start_test(const char *testname,
 						   "%s ", launcher);
 
 	snprintf(psql_cmd + offset, sizeof(psql_cmd) - offset,
-			 "python ./sql_isolation_testcase.py --dbname=\"%s\" < \"%s\" > \"%s\" 2>&1",
+			 "python ./sql_isolation_testcase.py --dbname=\"%s\" --initfile_prefix=\"%s\" < \"%s\" > \"%s\" 2>&1",
 			 dblist->str,
+			 outfile,
 			 infile,
 			 outfile);
 